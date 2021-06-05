9cf32bafd3ccf5e24f4918a6c7a896e00277587a
nikoelvambuena95 <38633487+nikoelvambuena95@users.noreply.github.com>
2021-06-04 22:16:38 -0700

    update flask API w/ SQL queries

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 # Import Flask
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, jsonify, render_template
 
 # Import SQL Alchemy
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.automap import automap_base
-from sqlalchemy import Column, Integer,Float, String,  create_engine
+from sqlalchemy import Column, Integer,Float, String, create_engine
 from sqlalchemy.orm import Session
+from sqlalchemy.sql import text
 
 #################################################
 # Database Setup
@@ -59,72 +60,66 @@ def routes():
     return(
         f"ICO public data API<br>"
         f"Available routes:<br>"
-        f"/api/v1.0/trade_statistics<br>"
-        f"/api/v1.0/total_production<br>"
+        f"/api/v1.0/export_countries<br>"
+        f"/api/v1.0/import_countries<br>"
         f"/api/v1.0/country_coordinates<br>"
     )
 
-@app.route("/api/v1.0/trade_statistics")
+@app.route("/api/v1.0/export_countries")
 def trade():
-    print("Server access: trade statistics data")
+    print("Server access: export country data")
     s = Session(bind=engine)
 
-    results = s.query(trade_stats.year, trade_stats.country, trade_stats.per1K_60kgbag,
-    trade_stats.transaction_type
-    ).all()
+    sql_query = text("SELECT tp.year AS year, tp.country AS country, cc.latitude AS latitude, cc.longitude AS longitude, tp.per1K_60kgbag AS production, ts.per1K_60kgbag AS export FROM Total_Production AS tp LEFT JOIN Trade_Stats AS ts ON tp.year=ts.year AND tp.country=ts.country LEFT JOIN Country_Coordinates AS cc ON tp.country=cc.country_name ORDER BY country ASC")
+    export_country_results = s.execute(sql_query).all()
 
     s.close()
 
-    trade_stats_list = []
-    for year, country, per1k_60kgbag, transaction_type in results:
-        trade_stats_dict = {}
-        trade_stats_dict['year'] = year
-        trade_stats_dict['country'] = country
-        trade_stats_dict['per1k_60kgbag'] = per1k_60kgbag
-        trade_stats_dict['transaction_type'] = transaction_type
-        trade_stats_list.append(trade_stats_dict)
+    export_countries_list = []
+    for row in export_country_results:
+        for i in row:
+            year = row[0]
+            country = row[1]
+            latitude = row[2]
+            longitude = row[3]
+            production = row[4]
+            export_1k = row[5]
+        export_countries_dict = {}
+        export_countries_dict['year'] = year
+        export_countries_dict['country'] = country
+        export_countries_dict['latitude'] = latitude
+        export_countries_dict['longitude'] = longitude
+        export_countries_dict['production'] = production
+        export_countries_dict['export_1k'] = export_1k
+        export_countries_list.append(export_countries_dict)
 
-    return jsonify(trade_stats_list)
+    return jsonify(export_countries_list)
 
-@app.route("/api/v1.0/total_production")
+@app.route("/api/v1.0/import_countries")
 def production():
-    print("Server access: total production per exporting country")
+    print("Server access: import country data")
     s = Session(bind=engine)
 
-    results = s.query(total_prod.year, total_prod.country, total_prod.per1K_60kgbag,
-    total_prod.month_group
-    ).all()
+    sql_query = text("SELECT ts.year AS year, ts.country AS country, cc.latitude AS latitude, cc.longitude AS longitude, ts.per1K_60kgbag AS import FROM Trade_Stats AS ts LEFT JOIN Country_Coordinates AS cc ON ts.country=cc.country_name WHERE ts.transaction_type='Import' AND latitude IS NOT NULL ORDER BY country ASC")
+    import_country_results = s.execute(sql_query).all()
 
-    total_prod_list = []
-    for id, year, country, per1k_60kgbag, month_group in results:
-        total_prod_dict = {}
-        total_prod_dict['year'] = year
-        total_prod_dict['country'] = country
-        total_prod_dict['per1k_60kgbag'] = per1k_60kgbag
-        total_prod_dict['month_group'] = month_group
-        total_prod_list.append(total_prod_dict)
+    import_countries_list = []
+    for row in import_country_results:
+        for i in row:
+            year = row[0]
+            country = row[1]
+            latitude = row[2]
+            longitude = row[3]
+            import_1k = row[4]
+        import_countries_dict = {}
+        import_countries_dict['year'] = year
+        import_countries_dict['country'] = country
+        import_countries_dict['latitude'] = latitude
+        import_countries_dict['longitude'] = longitude
+        import_countries_dict['import_1k'] = import_1k
+        import_countries_list.append(import_countries_dict)
     
-    return jsonify(total_prod_list)
-
-@app.route("/api/v1.0/country_coordinates")
-def location():
-    print("Server access: country coordinates")
-    s = Session(bind=engine)
-
-    results = s.query( coordinate.country_code, coordinate.latitude, coordinate.longitude,
-    coordinate.country_name
-    ).all()
-
-    coordinate_list = []
-    for country_code, latitude, longitude, country_name in results:
-        coordinate_dict = {}
-        coordinate_dict['country_code'] = country_code
-        coordinate_dict['latitude'] = latitude
-        coordinate_dict['longitude'] = longitude
-        coordinate_dict['country_name'] = country_name
-        coordinate_list.append(coordinate_dict)
-
-    return jsonify(coordinate_list)
+    return jsonify(import_countries_list)
 
 if __name__=="__main__":
     app.run(debug = True)
