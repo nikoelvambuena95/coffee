2dbc4af1260c83bef5ecb83f8f930797e89ed15b
nikoelvambuena95 <38633487+nikoelvambuena95@users.noreply.github.com>
2021-06-09 19:22:28 -0700

    initial framework

--- static/js/map.js
+++ static/js/map.js
@@ -1,15 +1,15 @@
 // Create map object 
 var myMap = L.map("map", {
     center: [45.52, -122.67],
-    zoom: 13
+    zoom: 3
   });
 
   // Create tile later
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
-    maxZoom: 8,
-    minZoom:5,
+    maxZoom: 10,
+    minZoom:3,
     zoomOffset: -1,
     id: "mapbox/light-v10",
     accessToken: API_KEY
@@ -24,14 +24,14 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     console.log(data[i].location),
     console.log(data[i].production)
 
-    // Add circles to the map 
-    L.circle(data[i].location, {
-      fillOpacity: 0.75,
-      color: 'white',
-      fillColor: 'red',
-      // Adjust radius
-      radius: data[i].production * 50
-    }).bindPopup("<h1>" + data[i].country + "</h1> <hr> <h3>Export:" + data[i].production + "</h3>").addTo(myMap);
+    // // Add circles to the map 
+    // L.circle(data[i].location, {
+    //   fillOpacity: 0.75,
+    //   color: 'white',
+    //   fillColor: 'red',
+    //   // Adjust radius
+    //   radius: data[i].production * 10
+    // }).bindPopup("<h1>" + data[i].country + "</h1> <hr> <h3>Export:" + data[i].production + "</h3>").addTo(myMap);
   };
   })
 
